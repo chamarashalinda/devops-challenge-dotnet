77ee96d4ca1181160c4b24100729e8d4604ca0fc
Chamara Shalinda <chamarashalinda@gmail.com>
2024-09-25 03:14:27 +0530

    Creating an index for TransactionId

---
+++ src/DevOpsChallenge.SalesApi.Database/Migrations/20240924213406_AddIndexOnTransactionId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevOpsChallenge.SalesApi.Database.Migrations
+{
+    public partial class AddIndexOnTransactionId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_TransactionId",
+                table: "Sale",
+                column: "TransactionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_TransactionId",
+                table: "Sale");
+        }
+    }
+}
